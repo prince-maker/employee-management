bf792d943d82e2b064d6cabef09c84c8d126fd81
Sunil <your-email@gmail.com>
2026-06-24 23:59:08 +0530

    Remove

--- src/main/java/com/example/employeemanagement/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagement.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/employeemanagement/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagement.repository;
-
-import com.example.employeemanagement.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
